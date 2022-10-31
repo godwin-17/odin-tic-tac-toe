8d3e9cd4d89e0a3ba9b8a89c45d5a4478ea46514
Godwin <agblezeg@gmail.com>
2022-10-31 17:45:33 +0100

    Add gameboard, player and game objects

---
+++ script.js
@@ -0,0 +1,43 @@
+const Gameboard = (() => {
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
+  
+  const cells = document.querySelectorAll(".game-cell");
+
+  gameboard.forEach((item, index) => {
+    cells[index].innerHTML = item;
+  });
+
+  return {gameboard};
+})();
+
+const Player = (name, mark) => {
+  return {name, mark};
+};
+
+const Game = (() => {
+  const {gameboard} = Gameboard;
+  const cells = document.querySelectorAll(".game-cell");
+
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
+
+  let turn = player1;
+
+  function checkTurn() {
+    turn === player1 ? turn = player2 : turn = player1;
+  }
+
+  function addMark() {
+
+    cells.forEach(cell => {
+      cell.addEventListener("click", () => {
+        cell.innerHTML = turn.mark;
+        gameboard[cell.dataset.index] = cell.innerHTML;
+        checkTurn();
+      });
+    });
+  }
+
+  addMark();
+
+})();
