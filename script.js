fe27723d90a1461453d90b37a8d568bece262eb7
Godwin <agblezeg@gmail.com>
2022-11-01 09:50:55 +0100

    Add winner function

--- script.js
+++ script.js
@@ -18,17 +18,43 @@ const Game = (() => {
   const {gameboard} = Gameboard;
   const cells = document.querySelectorAll(".game-cell");
 
-  const player1 = Player("Player 1", "X");
-  const player2 = Player("Player 2", "O");
+  const player1 = Player("Player 1", "O");
+  const player2 = Player("Player 2", "X");
 
-  let turn = player1;
+  const winningCombinations = [
+    // Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  function checkWinner() {
+    winningCombinations.forEach((combo) => {
+      let a = combo[0];
+      let b = combo[1];
+      let c = combo[2];
+
+      if (gameboard[a] !="" && gameboard[a] === gameboard[b] && gameboard[b] === gameboard[c]) {
+        console.log(`The winner is ${turn.name}`);
+      }
+    });
+  }
+
+  let turn = player2;
 
   function checkTurn() {
     turn === player1 ? turn = player2 : turn = player1;
+    checkWinner();
   }
 
   function addMark() {
-
     cells.forEach(cell => {
       cell.addEventListener("click", () => {
         if (cell.innerHTML != "") {
